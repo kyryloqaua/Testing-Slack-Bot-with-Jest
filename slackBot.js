8f10bc742a8c667bf9585986bd95a90d67c1d72d
Kyrylo Bielohryvtsev <kyryloqa@gmail.com>
2023-10-13 13:35:07 -0700

    added 2 more functions and tests

--- slackBot.js
+++ slackBot.js
@@ -1,14 +1,17 @@
 // slackBot.js
 
-const { WebClient } = require('@slack/web-api');
+const { WebClient, LogLevel } = require("@slack/web-api");
+const token = process.env.SLACK_BOT_TOKEN;
+
+const client = new WebClient(token, {
+  logLevel: LogLevel.DEBUG,
+});
 
 async function postMessageToSlack() {
-  const token = process.env.SLACK_BOT_TOKEN;
-  const web = new WebClient(token);
   const conversationId = 'C05UPN42E12';
 
   try {
-    const result = await web.chat.postMessage({
+    const result = await client.chat.postMessage({
       text: 'Hello world!',
       channel: conversationId,
     });
@@ -18,4 +21,59 @@ async function postMessageToSlack() {
   }
 }
 
-module.exports = postMessageToSlack;
+async function uploadFile() {
+  try {
+    const result = await client.files.uploadV2({
+      file: '/Users/bielohryvtsev/slack-bot/eyJ3IjoyMDQ4LCJoIjoyMDQ4LCJzY29wZSI6ImFwcCJ9.png',
+      filename: 'eyJ3IjoyMDQ4LCJoIjoyMDQ4LCJzY29wZSI6ImFwcCJ9.png',
+      channel_id: 'C05UPN42E12',
+      initial_comment: 'Here is the new logo.',
+    });
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function scheduleMessage() {
+  const channelId = 'C05UPN42E12';
+
+  try {
+    const today = new Date();
+    today.setHours(13, 35, 0, 0);
+
+    const result = await client.chat.scheduleMessage({
+      channel: channelId,
+      text: 'Scheduled message for 3 PM today',
+      post_at: today.getTime() / 1000,
+    });
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteMessage() {
+  const channelId = 'C05UPN42E12';
+  const messageId = '1697228872.524269';
+
+  try {
+    const result = await client.chat.delete({
+      channel: channelId,
+      ts: messageId,
+    });
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  postMessageToSlack,
+  uploadFile,
+  scheduleMessage,
+  deleteMessage,
+};
--- slackBot.test.js
+++ slackBot.test.js
@@ -1,32 +1,50 @@
 // slackBot.test.js
 
-const postMessageToSlack = require('./slackBot');
+const {
+  postMessageToSlack,
+  uploadFile,
+  scheduleMessage,
+  deleteMessage,
+} = require('./slackBot');
 
 describe('Slack Bot Message Posting', () => {
   test('Should successfully post a message to Slack and return a valid result', async () => {
     const result = await postMessageToSlack();
 
-    // Assertion Step 1: Check if the result is defined
     expect(result).toBeDefined();
-
-    // Assertion Step 2: Check if the 'ok' property is true
     expect(result.ok).toBe(true);
-
-    // Assertion Step 3: Check if the 'ts' property exists
     expect(result.ts).toBeDefined();
-
-    // Assertion Step 4: Check properties within the 'message' object
     expect(result.message).toBeDefined();
     expect(result.message.bot_id).toBe('B061A3Z07G9');
     expect(result.message.text).toBe('Hello world!');
-
-    // You can perform additional validation as needed
   });
+});
 
-  test('Should have a true "ok" property in the response', async () => {
-    const result = await postMessageToSlack();
+describe('Slack Bot File Upload', () => {
+  test('Should successfully upload a file to Slack and return a valid result', async () => {
+    const result = await uploadFile();
 
-    // Additional Test: Check if the 'ok' property in the response is true
+    expect(result).toBeDefined();
+    expect(result.ok).toBe(true);
+  });
+});
+
+describe('Slack Bot Scheduled Message', () => {
+  test('Should successfully schedule a message on Slack and return a valid result', async () => {
+    const result = await scheduleMessage();
+
+    expect(result).toBeDefined();
+    expect(result.ok).toBe(true);
+    expect(result.message.bot_id).toBe('B061A3Z07G9');
+    expect(result.message.text).toBe('Scheduled message for 3 PM today');
+  });
+});
+
+describe('Slack Bot Message Deletion', () => {
+  test('Should successfully delete a message on Slack and return a valid result', async () => {
+    const result = await deleteMessage();
+
+    expect(result).toBeDefined();
     expect(result.ok).toBe(true);
   });
 });
